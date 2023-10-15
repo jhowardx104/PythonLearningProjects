bb52e89819aef33c84b1524c8b6d26de7f150fd5
jhowardx104 <jhowardx104@gmail.com>
2023-10-15 11:10:29 -0400

    initial checkin

---
+++ data-types.py
@@ -0,0 +1,21 @@
+# Hello World
+print('Hello, World!')
+
+# ints
+myInt = -1
+print(myInt)
+
+if myInt > 0:
+    print('greater than 0')
+
+# floats
+myFloat = 7.0
+print(myFloat)
+
+mySecondFloat = float(7)
+
+if myFloat > 6.9:
+    print('Greater than 6.9')
+
+print(myInt + myFloat)
+print(myInt.__str__() + myFloat.__str__() + 'Hello')
---
+++ testing-collections.py
@@ -0,0 +1,8 @@
+# basic list
+myList = []
+myList.append(1)
+myList.append(2)
+myList.append(3)
+
+print(myList[1])
+print(myList)
