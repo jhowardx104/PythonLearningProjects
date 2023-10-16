f49382787007bc4e803640a84b27691881f4228b
jhowardx104 <jhowardx104@gmail.com>
2023-10-16 09:31:50 -0400

    Update writing-functions.py

--- writing-functions.py
+++ writing-functions.py
@@ -15,3 +15,14 @@ print_hello_world()
 print_user("jhoward", 32)
 result = power(3, 4)
 print(result)
+
+
+# functions with variable number of parameters
+def foo(first, second, third, *therest):
+    print("First: %s" % first)
+    print("Second: %s" % second)
+    print("Third: %s" % third)
+    print("Rest: %s" % list(therest))
+
+
+foo(1, 2, 3, 4, 5, 6)
