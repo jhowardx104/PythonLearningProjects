7e174bb4d30849004a9304041ced68a858392313
jhowardx104 <jhowardx104@gmail.com>
2023-11-22 19:52:35 -0500

    Update project

--- courses/BuildAMedicalDiagnosisBot/main.py
+++ courses/BuildAMedicalDiagnosisBot/main.py
@@ -10,18 +10,56 @@ def simulate_load():
     sleep(simulated_load)
 
 
+def assess_eyes(eyes):
+    if eyes == '1':
+        return no_dehydration
+    elif eyes == '2':
+        return severe_dehydration
+
+
+def assess_skin(skin):
+    if skin == '1':
+        return some_dehydration
+    elif skin == '2':
+        return no_dehydration
+
+
+def assess_appearance():
+    appearance = input(appearance_prompt)
+    if appearance == '1':
+        eyes = input(eyes_prompt)
+        return assess_eyes(eyes)
+    elif appearance == '2':
+        skin = input(skin_pinch_prompt)
+        return assess_skin(skin)
+
+
+def save_diagnosis(name, diagnosis):
+    print('Saving diagnosis (%s) for patient %s...' % (diagnosis, name))
+    simulate_load()
+    diagnosis_list.append({'name': name, 'diagnosis': diagnosis})
+
+
 def diagnose_new_patient():
     name = input(name_prompt)
-    print("Diagnosing new")
+    diagnosis = assess_appearance()
+    save_diagnosis(name, diagnosis)
 
 
 def view_patient_history():
-    print("Showing it all")
+    print('Loading patient history...\n\n')
+    simulate_load()
+    print('Patient - Diagnosis')
+    print('-------------------')
+    for patient in diagnosis_list:
+        print('%s - %s' % (patient['name'], patient['diagnosis']))
+    print('\n\nPress any key to continue...')
+    input()
 
 
 def app_ready():
     print('Application ready.')
-    while(True):
+    while True:
         choice = input(action_prompt)
         if choice == '1':
             diagnose_new_patient()
--- courses/BuildAMedicalDiagnosisBot/prompts.py
+++ courses/BuildAMedicalDiagnosisBot/prompts.py
@@ -6,3 +6,15 @@ action_prompt = ('What would you like to do?\n' +
                  '3. Quit application\n')
 invalid_action_message = "Invalid action selected. Please choose one of the following."
 name_prompt = 'What is the patient\'s name?'
+appearance_prompt = ('What is the patient\'s general appearance?\n' +
+                     '1. Normal appearance\n' +
+                     '2. Irritable or Lethargic\n')
+eyes_prompt = ('What best describes the patient\'s eyes?\n' +
+               '1. Normal or Slightly Sunken\n' +
+               '2. Very Sunken\n')
+skin_pinch_prompt = ('How fast did color return after a skin pinch test?\n' +
+                     '1. Returned quickly / at a normal speed\n' +
+                     '2. Return slowly\n')
+severe_dehydration = 'Severe dehydration'
+some_dehydration = 'Some dehydration'
+no_dehydration = 'No dehydration'
